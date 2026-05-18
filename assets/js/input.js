36d917d13c92017ae084cd4fa1d18b597f595580
Kohei Yoshida <was914hs@gmail.com>
2026-05-18 15:17:34 +0900

    password処理をlogin.jsに移動

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,28 +1,3 @@
-//全てのアイコン取得
-const eyeIcons = document.querySelectorAll(".eye-open, .eye-close");
-
-eyeIcons.forEach(btn => {
-    //btnがclickされた時に動く
-    btn.addEventListener('click', () => {
-
-        //clickされたiconから見て、「input-wrap」の中にあるinputを探す
-        const wrap = btn.closest('.input-wrap');
-        const input = wrap.querySelector('.input-pass');
-
-        //wrapの中にある「icon」を探す
-        const openIcon = wrap.querySelector('.eye-open');
-        const closeIcon = wrap.querySelector('.eye-close');
-
-        //条件分岐: password なら text に変更する
-        const isPassword = input.type === 'password'
-        input.type = isPassword ? 'text' : 'password';
-        //classを入れ替えて表示を切り替える
-        console.log('icon-open')
-        openIcon.classList.toggle('d-none', !isPassword);
-        closeIcon.classList.toggle('d-none', isPassword);
-    });
-});
-
 const getWeather = async (icons, layout) => {
     try {
         const url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json";
@@ -51,7 +26,6 @@ const getWeather = async (icons, layout) => {
         layout.forEach(item => {
             const el = document.createElement(item.tag);
             el.classList.add(item.class);
-            console.log('loading error');
 
             if (item.type === 'icon') {
                 el.src = matchedIcon.src;
@@ -63,7 +37,6 @@ const getWeather = async (icons, layout) => {
             }
             div.appendChild(el);
         });
-        console.log('icon, area, areaName, report');
         
         weatherTarget.appendChild(div);
         
